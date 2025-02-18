c6f6d41f31ccb2d5602d0b885fe98b92def994d7
ArthurVdoc <arthurviniciusocart@gmail.com>
2025-02-18 10:26:23 -0300

    adicionado herança múltipla no exercício 5

--- cpp-poo_ex5.cpp
+++ cpp-poo_ex5.cpp
@@ -8,6 +8,7 @@ class Vehicle{
     public:
         Vehicle( std::string bn, std::string md):brand(bn), model(md){}
         virtual void showData() = 0;
+        virtual ~Vehicle(){}
 };
 
 class Car:public Vehicle{
@@ -22,6 +23,28 @@ class Car:public Vehicle{
         }
 };
 
+class Eletronic{
+    public:
+        std::string B_brand; //Marca da bateria
+        int memory;
+        Eletronic(std::string ebn, int mr): B_brand(ebn), memory(mr){}
+};
+
+class EletricCar:public Vehicle, public Eletronic{
+    private:
+        int doorsNumber;
+    public:
+        EletricCar(std::string bn, std::string md, std::string ebn, int mr, int dn):
+            Vehicle(bn, md), Eletronic(ebn, mr), doorsNumber(dn){}
+        void showData() override {
+            std::cout<<"Marca.......: "<<brand<<std::endl
+                     <<"Modelo......: "<<model<<std::endl
+                     <<"Nº de Portas: "<<doorsNumber<<std::endl
+                     <<"Bateria.....: "<<B_brand<<std::endl
+                     <<"Memória.....: "<<memory<<std::endl;
+        }
+};
+
 void displayVehicleDetails(Vehicle* v){
     v->showData();
 }
@@ -29,10 +52,13 @@ void displayVehicleDetails(Vehicle* v){
 int main(){
     Car c1("Toyota", "Corolla", 4);
     Vehicle* c2 = new Car("Fiat", "Strada", 2);
+    EletricCar c3("BYD", "Seal", "Qualcoon", 256, 2);
 
     displayVehicleDetails(&c1);
-    std::cout<<std::endl;
+    std::cout << std::endl;
     displayVehicleDetails(c2);
+    std::cout << std::endl;
+    displayVehicleDetails(&c3);
 
     delete c2;
     return 0;  
